5e01e1b25bfcc998f4119f2fbfccf0d19a0cae6b
Sashie <boop@beep.bop>
2016-02-11 12:17:35 -0330

    Edited drawDot

--- src/main/java/ud/bi0/dragonSphereZ/skriptAPI/registers/registerEffects.java
+++ src/main/java/ud/bi0/dragonSphereZ/skriptAPI/registers/registerEffects.java
@@ -6,6 +6,6 @@ import ud.bi0.dragonSphereZ.skriptAPI.effects.*;
 public class registerEffects {
 
 	public static void DragonSphereZ() {
-		Skript.registerEffect(EffSimpleDot.class, new String[] { "drawDot [count %-number%], particle %string%[, material %-itemstack%][, speed %-number%][, ([offset]XYZ|RGB) %-number%, %-number%, %-number%], center %entity/location%, id %string%[, isSingle %-boolean%, %-player%][, r[ainbow]M[ode] %-boolean%], visibleRange %number%[, tps %-number%, second %-number%]" } );
+		Skript.registerEffect(EffSimpleDot.class, new String[] { "drawDot [count %-number%], particle %string%[, material %-itemstack%][, speed %-number%][, ([offset]XYZ|RGB) %-number%, %-number%, %-number%], center %entity/location%[, isSingle %-boolean%, %-player%][, r[ainbow]M[ode] %-boolean%], visibleRange %number%" } );
 	}
 }
