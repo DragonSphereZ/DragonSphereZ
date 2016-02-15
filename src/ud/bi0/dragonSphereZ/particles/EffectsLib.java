36697185991d67320fa8ba7023dba8e0f0561229
Sashie <boop@beep.bop>
2016-02-14 21:42:24 -0330

    Added the wing code for you to play with

--- src/ud/bi0/dragonSphereZ/particles/EffectsLib.java
+++ src/ud/bi0/dragonSphereZ/particles/EffectsLib.java
@@ -137,7 +137,99 @@ public class EffectsLib {
 	}
     
     
-    
+  //Add flapping wings in the future
+  	public static void drawWingsColor1(
+  			final String particle, 
+  			final Material dataMat, 
+  			final byte dataID, 
+  			final float speed, 
+  			final float offsetX, 
+  			final float offsetY, 
+  			final float offsetZ, 
+  			final Object center, 
+  			final String idName, 
+  			final boolean rainbowMode, 
+  			final float wingAngle, 
+  			final double visibleRange, 
+  			final boolean[][] shape, 
+  			final float height,
+  			final double space,  
+  			final long delayByTick,
+  			final List<Player> player) {
+  		if (!EffectsLib.arraylist1.containsKey(idName)) {
+  			int wings = Bukkit.getServer().getScheduler().runTaskTimer(DragonSphereCore.dragonSphereCore, new Runnable() {
+  						private float hue;
+  						Location location;
+  						@Override
+  						public void run() {
+  							//Location location = player.getLocation().clone();
+  							if (center instanceof Entity) {
+  								location = ((Entity) center).getLocation();
+  							}
+  							else if (center instanceof Location){
+  								location = new Location(((Location) center).getWorld(), ((Location) center).getX(), ((Location) center).getY(), ((Location) center).getZ());
+  							}
+  					        if (rainbowMode == true)
+  					        	//need to change hue to offsetX now
+  								hue += 0.01F;
+  								hue = (hue >= 1.0F ? 0.0F : hue);
+  					        double x;
+  					        final double defX = x = location.getX() + space;
+  					        double y = location.clone().getY() + 2.7 + height;
+  					        double y2 = location.clone().getY() + 2.7 + height;
+  					        for (int i = 0; i < shape.length; ++i) {
+  					            for (int j = 0; j < shape[i].length; ++j) {
+  					                if (shape[i][j]) {
+  					                    final Location target = location.clone();
+  					                    target.setX(x);
+  					                    target.setY(y);
+  					                    Vector vR = target.toVector().subtract(location.toVector());
+  					                    final Vector v2 = VectorUtils.getBackVector(location);
+  					                    double rightWing = Math.toRadians(location.getYaw() + 90.0f - wingAngle);
+  					                    vR = VectorUtils.rotateAroundAxisY(vR, -rightWing);
+  		                                v2.setY(0).multiply(-0.2);
+  					                    location.add(vR);
+  					                    location.add(v2);
+  					                    //for (int k = 0; k < 2; ++k) {
+  					                    ParticleEffect.valueOf(particle).display(dataMat, dataID, player, location, visibleRange, rainbowMode, offsetX, offsetY, offsetZ, speed, 1);
+  					                    //}
+  					                    location.subtract(v2);
+  					                    location.subtract(vR);
+  					                }
+  					                x += space;
+  					            }
+  					            y -= space;
+  					            x = defX;
+  					        }
+  					        for (int i2 = 0; i2 < shape.length; ++i2) {
+  					            for (int j2 = 0; j2 < shape[i2].length; ++j2) {
+  					                if (shape[i2][j2]) {
+  					                    final Location target = location.clone();
+  					                    target.setX(x);
+  					                    target.setY(y2);
+  					                    Vector vL = target.toVector().subtract(location.toVector());
+  					                    final Vector v2 = VectorUtils.getBackVector(location);
+  					                    double leftWing = Math.toRadians(location.getYaw() + 90.0f + wingAngle);
+  					                    vL = VectorUtils.rotateAroundAxisY(vL, -leftWing);
+  		                                v2.setY(0).multiply(-0.2);
+  					                    location.add(vL);
+  					                    location.add(v2);
+  					                    //for (int k = 0; k < 2; ++k) {
+  					                    ParticleEffect.valueOf(particle).display(dataMat, dataID, player, location, visibleRange, rainbowMode, offsetX, offsetY, offsetZ, speed, 1);
+  					                    //}
+  					                    location.subtract(v2);
+  					                    location.subtract(vL);
+  					                }
+  					                x += space;
+  					            }
+  					            y2 -= space;
+  					            x = defX;
+  					        }
+  						}
+  					}, 1, delayByTick).getTaskId();//2,1
+  				EffectsLib.arraylist1.put((idName), wings);
+  			}
+  		}
     
 	
 }
